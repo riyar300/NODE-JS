d3f750d1259a5fab56d9f537bd193e903c6e9b6c
Riya Roy <riyar300@gmail.com>
2024-03-23 13:36:55 +0530

    DAY 3 ON TEMPLATING ENGINES

---
+++ DAY 3/PUG AND HANDLEBARS/app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const expressHbs = require('express-handlebars');
+
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+app.set('views', 'views')
+
+//app.set('view engine', 'pug');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render(404,{docTitle : '404'});
+});
+
+app.listen(3000);
---
+++ DAY 3/PUG AND HANDLEBARS/routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminRoutes = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products =  adminRoutes.products;
+  res.render('shop', {prods : products, docTitle : 'Shop', hasProducts : products.length > 0});
+});
+
+module.exports = router;
