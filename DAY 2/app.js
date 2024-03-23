f741c4d92b6dfa750309d116076c8f276c05a338
Riya Roy <riyar300@gmail.com>
2024-03-23 14:09:23 +0530

    DAY 2 MINOR CHANGES

--- DAY 2/app.js
+++ DAY 2/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/userRoutes');
@@ -8,6 +9,7 @@ const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use(userRoutes);
 app.use(adminRoutes);
